649302afc2f26cc0fa5c8ed7810c77b08af17cb8
Васильев Владимир <vovas0712@gmail.com>
2021-12-14 17:43:16 +0600

    добавление фотки

--- SchoolBase54/AddTeacher.xaml.cs
+++ SchoolBase54/AddTeacher.xaml.cs
@@ -1,6 +1,8 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Microsoft.Win32;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,15 +32,41 @@ namespace SchoolBase54
             MainWindow ma = new MainWindow();
             ma.Show();
         }
+        public void ImageClick(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Image files (*.png, *.jpg)|*.png;*.jpg";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == true)
+            {
+
+                string path = openFileDialog1.FileName;
+                imagepath.Content = path;
+                //...
+            }
+        }
+
         public void ReadyClick(object sender, EventArgs e)
         {
+            string FileName = imagepath.Content.ToString();
+            byte[] ImageData;
+            FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            ImageData = br.ReadBytes((int)fs.Length);
+            br.Close();
+            fs.Close();
             DB db = new DB();
-            MySqlCommand command = new MySqlCommand("INSERT INTO `workers` (`name`, `surname`, `fathername`, `category`, `iin`) VALUES (@uN, @uS, @uF, @uG, @uI)", db.getConnection());
+            MySqlCommand command = new MySqlCommand("INSERT INTO `workers` (`name`, `surname`, `fathername`, `category`, `iin`, `image`) VALUES (@uN, @uS, @uF, @uG, @uI, @uP)", db.getConnection());
             command.Parameters.Add("@uN", MySqlDbType.VarChar).Value = NameTextBox.Text;
             command.Parameters.Add("@uS", MySqlDbType.VarChar).Value = SurnameTextBox.Text;
             command.Parameters.Add("@uF", MySqlDbType.VarChar).Value = OtceTextBox.Text;
             command.Parameters.Add("@uG", MySqlDbType.VarChar).Value = CategoryTextBox.Text;
             command.Parameters.Add("@uI", MySqlDbType.VarChar).Value = IINTextBox.Text;
+            command.Parameters.Add("@uP", MySqlDbType.Blob).Value = ImageData;
             db.openConnection();
             if (command.ExecuteNonQuery() == 1)
             {
